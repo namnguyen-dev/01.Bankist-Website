f1ef476f7763335fde3e6cb03f57b4f95f478b8a
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-02-24 17:38:16 +0700

    added Sticky nav

--- script.js
+++ script.js
@@ -116,3 +116,11 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5)
 );
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//  Sticky navigation
+const initialCoords = section1.getBoundingClientRect()
+
+window.addEventListener('scroll', function(e) {
+  if(window.scrollY > initialCoords.top) nav.classList.add('sticky')
+  else nav.classList.remove('sticky');
+})
